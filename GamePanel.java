1f44a9a6266a5e78f9566c931edcbba36d7e87d0
alyannapauline24 <alyannapauline24@github.com>
2016-05-14 09:59:48 +0800

    Updated GUI. Added turn choice of player.

--- GamePanel.java
+++ GamePanel.java
@@ -1,20 +1,13 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Point;
+import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+import java.util.*;
+import javax.swing.*;
 
 
 public class GamePanel extends JPanel implements ActionListener{
     private static JFrame frame;
+    private JPanel content;
+    private JPanel startPanel;
     private JButton b[][] = new JButton[3][3];
     private State currentState;
     private int turn;
@@ -25,45 +18,83 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void createAndShowGui() {
-        // Create the tiles
-        this.removeAll();
-
-        resetState();
-        drawBoard();
-
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                b[i][j].addActionListener(this);
-                add(b[i][j]);
-            }
-        }
-
         // Create the "New Game" button
         JButton newGameButton = new JButton();
         newGameButton.setFont(new Font("Arial", Font.PLAIN, 30));
-        newGameButton.setBackground(new Color(64, 224, 208));
+        newGameButton.setBackground(new Color(0, 100, 0));
+        newGameButton.setForeground(Color.white);
         newGameButton.setText("New Game");
         newGameButton.setPreferredSize(new Dimension(500, 50));
         newGameButton.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent ev){
-                // Reset state here
+                frame.dispose();
+                GamePanel game = new GamePanel();
+                game.createAndShowGui();
             }
             public void mousePressed(MouseEvent ev){}
             public void mouseEntered(MouseEvent ev){}
             public void mouseReleased(MouseEvent ev){}
             public void mouseExited(MouseEvent ev){}
-
         });
 
-        // Create the Board Panel Layout
-        this.setLayout(new GridLayout(3,3));
-        this.setOpaque(true);
-        this.setBackground(new Color(224, 255, 255));
+        // Creates the starting panel
+        JButton p1Button = new JButton();
+        JButton p2Button = new JButton();
+        JLabel turnLabel = new JLabel("Choose your turn:");
+        turnLabel.setFont(new Font("Arial", Font.PLAIN, 30));
+        turnLabel.setPreferredSize(new Dimension(200, 200));
+        turnLabel.setForeground(Color.white);
+        p1Button.setFont(new Font("Arial", Font.PLAIN, 20));
+        p1Button.setBackground(new Color(250, 250, 250));
+        p1Button.setText("Player 1");
+        p1Button.setPreferredSize(new Dimension(200, 50));
+        p1Button.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent ev){
+                turn = 1;
+                content.remove(startPanel);
+                content.revalidate();
+                content.repaint();
+                createBoardGui();
+            }
+            public void mousePressed(MouseEvent ev){}
+            public void mouseEntered(MouseEvent ev){}
+            public void mouseReleased(MouseEvent ev){}
+            public void mouseExited(MouseEvent ev){}
+        });
+
+        p2Button.setFont(new Font("Arial", Font.PLAIN, 20));
+        p2Button.setBackground(new Color(250, 250, 250));
+        p2Button.setText("Player 2");
+        p2Button.setPreferredSize(new Dimension(200, 50));
+        p2Button.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent ev){
+                turn = 2;
+                content.remove(startPanel);
+                content.revalidate();
+                content.repaint();
+                createBoardGui();
+            }
+            public void mousePressed(MouseEvent ev){}
+            public void mouseEntered(MouseEvent ev){}
+            public void mouseReleased(MouseEvent ev){}
+            public void mouseExited(MouseEvent ev){}
+        });
+
+        JPanel buttonHolder = new JPanel();
+        buttonHolder.setBackground(Color.black);
+        buttonHolder.add(p1Button);
+        buttonHolder.add(p2Button);
+
+        startPanel = new JPanel();
+        startPanel.setBackground(Color.black);
+        startPanel.setLayout(new BorderLayout());
+        startPanel.add(turnLabel, BorderLayout.NORTH);
+        startPanel.add(buttonHolder, BorderLayout.CENTER);
 
         // Creates the content panel
-        JPanel content = new JPanel();
+        content = new JPanel();
         content.setLayout(new BorderLayout());
-        content.add(this, BorderLayout.CENTER);
+        content.add(startPanel, BorderLayout.CENTER);
         content.add(newGameButton, BorderLayout.SOUTH);
 
         // Creates the window
@@ -74,6 +105,31 @@ public class GamePanel extends JPanel implements ActionListener{
         frame.setResizable(false);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void createBoardGui() {
+        System.out.println(turn);
+
+        // Create the tiles
+        this.removeAll();
+
+        resetState();
+        drawBoard();
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                b[i][j].setForeground(Color.white);
+                b[i][j].addActionListener(this);
+                add(b[i][j]);
+            }
+        }
+
+        // Create the Board Panel Layout
+        this.setLayout(new GridLayout(3,3));
+        this.setOpaque(true);
+        this.setBackground(new Color(3, 3, 3));
+
+        content.add(this, BorderLayout.CENTER);
     }
 
 
@@ -128,7 +184,7 @@ public class GamePanel extends JPanel implements ActionListener{
             for(int j = 0; j < 3; j++){
                 b[i][j] = new JButton();
                 b[i][j].setFont(new Font("Arial", Font.PLAIN, 40));
-                b[i][j].setBackground(new Color(64, 224, 208));
+                b[i][j].setBackground(new Color(3, 3, 3));
             }
         }
     }
